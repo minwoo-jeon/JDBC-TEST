487ffcc45cbf679d0faf247cd58d9f998c3d123a
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-10-21 19:51:56 +0900

    add : DataSource

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -10,13 +10,14 @@ import static hello.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class DBConnectionUtil {
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         try {
-           Connection connection =  DriverManager.getConnection(URL,USERNAME,PASSWORD);
-            log.info("get connection = {} , class={}" , connection, connection.getClass());
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); //커넥션 얻어오는 작업
+            log.info("get connection = {} , class={}", connection,connection.getClass() );
             return connection;
         } catch (SQLException e) {
             throw new IllegalStateException(e);
         }
+
     }
 }
--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -14,6 +14,28 @@ import java.util.NoSuchElementException;
 @Slf4j
 public class MemberRepositoryV0 {
 
+    public void update(String member , int money)throws Exception{
+        String sql = "update member set money = ? where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection(); //DB와 연결하고 h2데이터베이스 커넥션 객체를 반환받는다
+            pstmt = con.prepareStatement(sql); // SQL쿼리를 넘겨주고, 해당 쿼리를 관리할 PreparedStatement 객체를 받아온다/
+            pstmt.setInt(1, money); // 첫번쨰 ? 에 파라미터를 동적으로 바인딩해준다
+            pstmt.setString(2,member);
+            int resultSize = pstmt.executeUpdate(); //바인딩된 sql쿼리를  요청
+            log.info("resultSize={}" , resultSize);
+        }catch (SQLException e){
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(con,pstmt,null);
+        }
+    }
+
+
     public Member save(Member member)throws Exception{
         String sql = "insert into member(member_id , money) values(?,?)";
 
@@ -27,6 +49,24 @@ public class MemberRepositoryV0 {
             pstmt.setInt(2,member.getMoney());
             pstmt.executeUpdate();
             return member;
+        }catch (SQLException e){
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(con,pstmt,null);
+        }
+    }
+
+    public void delete(String memberId) throws Exception{
+        String sql = "delete from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
         }catch (SQLException e){
             log.error("db error",e);
             throw e;
